001b869414860e8cb07a09b27a431936752fd565
avy60263xv <avy60263xv@gmail.com>
2021-01-25 23:32:31 +0800

    mouse move

---
+++ mouse.js
@@ -0,0 +1,16 @@
+//Window.innerWidth
+//Window.innerHeight
+
+const cat = document.getElementById("cat_astronaut");
+
+let mouse = {
+    x:0,
+    y:0
+}
+window.addEventListener('mousemove', function(e){
+    console.log(`${e.pageX},${e.pageY}`);
+    mouse.x = e.pageX;
+    mouse.y = e.pageY;
+    cat.style.left = mouse.x + "px"; //cat 左右移
+    cat.style.top = mouse.y + "px";  //cat 上下移   
+});
